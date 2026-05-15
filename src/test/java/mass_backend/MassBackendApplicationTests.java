5b4f9e1cc7f573bb708ce211f335395179c497cc
SH4M1R <jpshaiz123@gmail.com>
2026-05-14 20:00:08 -0500

    primer commit

---
+++ src/test/java/mass_backend/MassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mass_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
