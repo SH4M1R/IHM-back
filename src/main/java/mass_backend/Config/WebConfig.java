b1ac508a8449b10913784e3b5bdd34f8d0416854
SH4M1R <jpshaiz123@gmail.com>
2026-06-25 16:13:31 -0500

    asignacion de cors

--- src/main/java/mass_backend/Config/WebConfig.java
+++ src/main/java/mass_backend/Config/WebConfig.java
@@ -4,31 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebConfig {
 
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-
         return new WebMvcConfigurer() {
-
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:3000"
-                        )
-                        .allowedMethods(
-                                "GET",
-                                "POST",
-                                "PUT",
-                                "DELETE",
-                                "OPTIONS"
-                        )
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                registry.addMapping("/**") // Aplica a todos los endpoints del backend
+                        .allowedOrigins(allowedOrigins) // Lee el http://localhost:3000
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos los encabezados
+                        .allowCredentials(true); // Por si manejas cookies o sesiones en el futuro
             }
         };
     }
